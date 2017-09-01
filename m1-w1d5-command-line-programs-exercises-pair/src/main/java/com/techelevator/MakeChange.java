312777cdc7169ceb6f43dccf3781a4071d22f7f2
Richard Hresko <richardhresko@gmail.com>
2017-09-01 15:38:15 -0400

    change team exercise

--- m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
+++ m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
  
@@ -12,6 +14,18 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 
+	
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter your bill amount");
+			double billAmount = input.nextDouble();
+			input.nextLine();
+		
+		System.out.println("How much was tendered?");
+			double tendered = input.nextDouble();
+			input.nextLine();
+		
+		double change = tendered - billAmount;
+		System.out.println(change);
+		}
 	}
 
-}
